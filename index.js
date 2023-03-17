f1bd05d8420d1b06fc419784c38fd6e437902768
Kadimane <kadimanegloriah@gmail.com>
2023-03-17 20:45:36 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result;
+   if(distance<400){
+    result='This one is on me!';
+   }
+   else if((distance>2000) && (distance<2501)){
+      result='I will gladly take your thirty bucks.';
+      }
+      else{
+          result='No can do.';
+        }
+      
+   
+   return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  result=city==='NYC'?'Ok, sounds good.':'No go.';
+  return result;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(res){
+  let result;
+  switch(res){
+    case 'generous':
+      result='Thank you so much.';
+      break;
+
+    case 'not as generous':
+      result='Thank you.';
+      break;
+    default:
+      result='Bye.';
+  }
+  return result;
 }
